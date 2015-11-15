ea6f5250ba0a070684d2be99f9b98fbcce3d5a6f
Scott Chamberlain <myrmecocystus@gmail.com>
2015-11-14 16:25:47 -0800

    fixed as.cassette

--- R/as-casettes.R
+++ R/as-casettes.R
@@ -1,4 +1,6 @@
 #' Coerce names, etc. to cassettes
+#'
+#' @param x Input
 #' @export
 as.cassette <- function(x) UseMethod("as.cassette")
 
@@ -6,7 +8,13 @@ as.cassette <- function(x) UseMethod("as.cassette")
 as.cassette.cassette <- function(x) x
 
 #' @export
-as.cassette.character <- function(x) if(is_path(x)) read_cassette_meta(x) else read_cassette_meta(get_cassette_path(x))
+as.cassette.character <- function(x) {
+  if (is_path(x)) {
+    read_cassette_meta(x)
+  } else {
+    read_cassette_meta(get_cassette_path(x))
+  }
+}
 
 #' @export
 as.cassette.cassettepath <- function(x) read_cassette_meta(x)
@@ -16,11 +24,19 @@ as.cassette.cassettepath <- function(x) read_cassette_meta(x)
 as.cassette.list <- function(x) lapply(x, as.cassette)
 
 #' Coerce to a cassette path
+#'
 #' @export
+#' @rdname as.cassette
 as.cassettepath <- function(x) UseMethod("as.cassettepath")
 
 #' @export
-as.cassettepath.character <- function(x) if(file.exists(x)) structure(x, class="cassettepath") else stop("Path not found")
+as.cassettepath.character <- function(x) {
+  if (file.exists(x)) {
+    structure(x, class = "cassettepath")
+  } else {
+    stop("Path not found", call. = FALSE)
+  }
+}
 
 #' @export
 print.cassettepath <- function(x) cat(paste0("<cassette path>"), x[[1]])
--- R/vcr-package.r
+++ R/vcr-package.r
@@ -2,6 +2,7 @@
 #'
 #' @importFrom digest digest
 #' @importFrom R6 R6Class
+#' @importFrom httr http_condition
 #' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
 #' @docType package
 #' @aliases vcr-package
