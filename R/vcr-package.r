c07f33dbb65503b5a9dad40c0f94f6b12ab387a5
Scott Chamberlain <myrmecocystus@gmail.com>
2015-11-14 16:27:22 -0800

    import yaml and httr

--- R/vcr-package.r
+++ R/vcr-package.r
@@ -3,6 +3,7 @@
 #' @importFrom digest digest
 #' @importFrom R6 R6Class
 #' @importFrom httr http_condition
+#' @importFrom yaml yaml.load_file
 #' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
 #' @docType package
 #' @aliases vcr-package
