8e1debd61481da4da797e6336f76bfb9fd59d341
Scott Chamberlain <myrmecocystus@gmail.com>
2019-12-12 11:05:50 -0800

    #128 fix httr handling of images when not writing to disk, bump version, require newest webmockr on github

--- R/serialize_to_httr.R
+++ R/serialize_to_httr.R
@@ -1,3 +1,9 @@
+disk_true <- function(x) {
+  if (is.null(x)) return(FALSE)
+  assert(x, "logical")
+  return(x)
+}
+
 # generate actual httr response
 serialize_to_httr <- function(request, response) {
   # request
@@ -19,7 +25,7 @@ serialize_to_httr <- function(request, response) {
   # in vcr >= v0.4, "disk" is in the response, but in older versions
   # its missing - use response$body if disk is not present
   response_body <- response$body
-  if ("disk" %in% names(response) && !is.null(response$disk)) {
+  if ("disk" %in% names(response) && disk_true(response$disk)) {
     response_body <- if (response$disk) {
       structure(response$body, class = "path")
     } else {
