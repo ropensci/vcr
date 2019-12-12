3b37b72794616c89e12973618d443b02de3afecf
Scott Chamberlain <myrmecocystus@gmail.com>
2019-12-12 11:16:35 -0800

    change image test file to httr and crul tests

---
+++ tests/testthat/test-binary_images.R
@@ -0,0 +1,63 @@
+context("handling images: httr")
+
+tmpdir <- tempdir()
+vcr_configure(dir = tmpdir)
+
+test_that("use_cassette w/ with images: httr", {
+  skip_on_cran()
+  library(httr)
+
+  url <- "https://httpbin.org/image/jpeg"
+
+  # works on 1st request - doing a real http request
+  use_cassette("test_write_httr_binary_img", {
+    out <- httr::GET(url)
+  })
+
+  expect_is(out, "response")
+  expect_is(out$content, "raw")
+  expect_is(httr::content(out), "array")
+
+  # works on 2nd request - using cassette
+  use_cassette("test_write_httr_binary_img", {
+    out2 <- httr::GET(url)
+  })
+  expect_is(out2, "response")
+  expect_is(out2$content, "raw")
+  expect_is(httr::content(out2), "array")
+
+  expect_identical(httr::content(out), httr::content(out2))
+})
+
+context("handling images: crul")
+
+test_that("use_cassette w/ with images: crul", {
+  skip_on_cran()
+  library(crul)
+
+  url <- "https://httpbin.org/image/jpeg"
+
+  # works on 1st request - doing a real http request
+  use_cassette("test_write_crul_binary_img", {
+    out <- crul::HttpClient$new(url)$get()
+  })
+
+  expect_is(out, "HttpResponse")
+  expect_is(out$content, "raw")
+
+  # works on 2nd request - using cassette
+  use_cassette("test_write_crul_binary_img", {
+    out2 <- crul::HttpClient$new(url)$get()
+  })
+  expect_is(out2, "HttpResponse")
+  expect_is(out2$content, "raw")
+
+  expect_identical(out$content, out2$content)
+})
+
+# cleanup
+files <- c("test_write_httr_binary_img.yml", "test_write_crul_binary_img.yml")
+unlink(file.path(vcr_configuration()$dir, files))
+
+# reset configuration
+vcr_configure_reset()
