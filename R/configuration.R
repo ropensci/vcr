95aaec799a36e3eefed45661662c23ccc75edba6
Aaron Wolen <aaron@wolen.com>
2020-02-02 13:57:49 -0600

    document config params in a list

--- R/configuration.R
+++ R/configuration.R
@@ -1,43 +1,65 @@
 #' Configuration
 #'
-#' @export
-#' @param dir Cassette directory
-#' @param record (character) One of 'all', 'none', 'new_episodes', or 'once'.
+#' Optional settings to customize vcr's default behavior.
+#'
+#' @param ... configuration settings used to override defaults. See below for a
+#'   complete list of valid arguments.
+#'
+#' ## Casstte options
+#'
+#' - `dir` Cassette directory
+#' - `record` (character) One of 'all', 'none', 'new_episodes', or 'once'.
 #' See [recording]
-#' @param match_requests_on vector of matchers. Default: (`method`, `uri`)
+#' - `match_requests_on` vector of matchers. Default: (`method`, `uri`)
 #' See [request-matching] for details.
-#' @param allow_unused_http_interactions (logical) Default: `TRUE`
-#' @param serialize_with (character) only option is "yaml"
-#' @param persist_with (character) only option is "FileSystem"
-#' @param ignore_hosts (character) Vector of hosts to ignore. e.g., localhost, or
-#' google.com. These hosts are ignored and real HTTP requests allowed to go
-#' through
-#' @param ignore_localhost (logical) Default: `FALSE`
-#' @param ignore_request List of requests to ignore. NOT USED RIGHT NOW, sorry
-#' @param uri_parser the uri parser, default: [crul::url_parse()]
-#' @param preserve_exact_body_bytes (logical) preserve exact body bytes for
-#' @param turned_off (logical) VCR is turned on by default. Default:
+#' - `cassettes` (list) don't use
+#' - `linked_context` (logical) linked context
+#'
+#' ## Recording details
+#'
+#' - `serialize_with`: (character) only option is "yaml"
+#' - `persist_with` (character) only option is "FileSystem"
+#' - `uri_parser` the uri parser, default: [crul::url_parse()]
+#' - `preserve_exact_body_bytes` (logical) preserve exact body bytes for
+#' - `turned_off` (logical) VCR is turned on by default. Default:
 #' `FALSE`
-#' @param re_record_interval (numeric) When given, the cassette will be
+#' - `re_record_interval` (numeric) When given, the cassette will be
 #' re-recorded at the given interval, in seconds.
-#' @param clean_outdated_http_interactions (logical) Should outdated interactions
+#' - `clean_outdated_http_interactions` (logical) Should outdated interactions
 #' be recorded back to file. Default: `FALSE`
-#' @param allow_http_connections_when_no_cassette (logical) Determines how vcr
+#' - `write_disk_path` (character) path to write files to
+#' for any requests that write responses to disk. by default this parameter
+#' is `NULL`. For testing a package, you'll probably want this path to
+#' be in your `tests/` directory, perhaps next to your cassettes
+#' directory, e.g., where your cassettes are in `tests/fixtures`, your
+#' files from requests that write to disk are in `tests/files`
+#'
+#' ## Connectivity
+#'
+#' - `allow_unused_http_interactions` (logical) Default: `TRUE`
+#' - `allow_http_connections_when_no_cassette` (logical) Determines how vcr
 #' treats HTTP requests that are made when no vcr cassette is in use. When
 #' `TRUE`, requests made when there is no vcr cassette in use will be allowed.
 #' When `FALSE` (default), an [UnhandledHTTPRequestError] error will be raised
 #' for any HTTP request made when there is no cassette in use
-#' @param cassettes (list) don't use
-#' @param linked_context (logical) linked context
-#' @param log (logical) should we log important vcr things? Default: `FALSE`
-#' @param log_opts (list) Additional logging options. Options include:
 #'
-#' - file: one of a file path to log to or "console"
-#' - log_prefix: default: "Cassette". We insert the cassette name after
-#' that prefix, then the rest of the message
-#' - more to come
+#' ## Logging
 #'
-#' @param filter_sensitive_data (list) named list of values to replace. format
+#' - `log` (logical) should we log important vcr things? Default: `FALSE`
+#' - `log_opts` (list) Additional logging options. Options include:
+#'   - file: one of a file path to log to or "console"
+#'   - log_prefix: default: "Cassette". We insert the cassette name after
+#'     that prefix, then the rest of the message
+#'   - More to come...
+#'
+#' ## Filtering
+#'
+#' - `ignore_hosts` (character) Vector of hosts to ignore. e.g., localhost, or
+#' google.com. These hosts are ignored and real HTTP requests allowed to go
+#' through
+#' - `ignore_localhost` (logical) Default: `FALSE`
+#' - `ignore_request` List of requests to ignore. NOT USED RIGHT NOW, sorry
+#' - `filter_sensitive_data` (list) named list of values to replace. format
 #' is: `list(thing_to_replace_it_with = thing_to_replace)`. We replace all
 #' instances of `thing_to_replace` with `thing_to_replace_it_with`. Before
 #' recording (writing to a cassette) we do the replacement and then when
@@ -46,12 +68,6 @@
 #' function `write_interactions()`, while before playback replacement
 #' happens in internal function `YAML$deserialize_path()`
 #'
-#' @param write_disk_path (character) path to write files to
-#' for any requests that write responses to disk. by default this parameter
-#' is `NULL`. For testing a package, you'll probably want this path to
-#' be in your `tests/` directory, perhaps next to your cassettes
-#' directory, e.g., where your cassettes are in `tests/fixtures`, your
-#' files from requests that write to disk are in `tests/files`
 #'
 #' @examples
 #' vcr_configure(dir = tempdir())
