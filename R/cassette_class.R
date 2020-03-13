9c32d6ffd0dff1c25ee97d17e35d498733639609
Scott Chamberlain <myrmecocystus@gmail.com>
2020-03-13 15:36:29 -0700

    #135 use relative path instead of absolute full path for write to disk
    change made in make_http_interaction method within the Cassette class
    we were using normalizePath, leading to a full path
    which only worked on the machine the test/fixture/file was originally run/created on
    no longer normalizing path

--- R/cassette_class.R
+++ R/cassette_class.R
@@ -630,7 +630,6 @@ Cassette <- R6::R6Class(
           if (is.null(write_disk_path))
             stop("if writing to disk, write_disk_path must be given; ",
               "see ?vcr_configure")
-          write_disk_path <- normalizePath(write_disk_path, mustWork=TRUE)
           new_file_path <- file.path(write_disk_path, basename(x$content))
         }
       }
