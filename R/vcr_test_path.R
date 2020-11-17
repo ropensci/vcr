3b172b1e7df7ae9eeff4f328bbfdbc71c0b4504f
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-11-17 09:02:15 +0100

    add link

--- R/vcr_test_path.R
+++ R/vcr_test_path.R
@@ -10,6 +10,7 @@
 #'
 #' @examples
 #' vcr_test_path("fixtures")
+# Adapted from https://github.com/r-lib/testthat/blob/45a9c705402bd51af29b9d999e587ba789f6203f/R/test-path.R#L1
 vcr_test_path <- function(...) {
   if (identical(Sys.getenv("TESTTHAT"), "true") && !isTRUE(getOption("testthat_interactive"))) {
     if (missing(...)) {
