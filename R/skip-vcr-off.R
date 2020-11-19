8bc48daccb8bf7a42b292a9913da8d517eb69485
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-11-19 09:57:21 +0100

    better syntax

--- R/skip-vcr-off.R
+++ R/skip-vcr-off.R
@@ -16,7 +16,7 @@
 #' @seealso [turn_off()]
 skip_if_vcr_off <- function() {
 
-  if (inherits(try(find.package("testthat"), silent = TRUE), "try-error")) {
+  if (!requireNamespace("testthat", quietly = TRUE)) {
     stop(
       paste0(
         "This function is meant to be use within testthat tests.",
