7270a237c1d752b409e847fd0588a00f46b1ba83
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-11-22 15:03:18 +0100

    review feedback

--- R/vcr_test_path.R
+++ R/vcr_test_path.R
@@ -3,7 +3,9 @@
 #' This function, similar to `testthat::test_path()`, is designed to work both
 #' interactively and during tests, locating files in the `tests/` directory.
 #'
-#' @param ...	Character vectors giving path component.
+#' @param ...	Character vectors giving path component. each character string
+#' gets added on to the path, e.g., `vcr_test_path("a", "b")` becomes
+#' `tests/a/b` when run from the root of the package.
 #'
 #' @return A character vector giving the path
 #' @export
@@ -13,7 +15,13 @@
 #' vcr_test_path("fixtures")
 # Adapted from https://github.com/r-lib/testthat/blob/45a9c705402bd51af29b9d999e587ba789f6203f/R/test-path.R#L1
 vcr_test_path <- function(...) {
-  if (identical(Sys.getenv("TESTTHAT"), "true") && !isTRUE(getOption("testthat_interactive"))) {
+
+  if (any(!nzchar(...))) {
+    stop("Please use non empty path elements.")
+  }
+
+  if (identical(Sys.getenv("TESTTHAT"), "true") &&
+      !isTRUE(getOption("testthat_interactive"))) {
     if (missing(...)) {
       "../."
     }
