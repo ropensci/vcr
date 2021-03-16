fe623fc85c1bc066d8660ebab78b85855f3b2f21
Maëlle Salmon <maelle.salmon@yahoo.se>
2021-03-16 12:52:19 +0100

    Update vcr_test_path.R

--- R/vcr_test_path.R
+++ R/vcr_test_path.R
@@ -24,7 +24,7 @@ vcr_test_path <- function(...) {
     if (missing(...)) {
       "../."
     } else {
-      here::here("..", ...)
+       file.path("..", ...)
     }
   } else {
     path <- here::here("tests", ...)
