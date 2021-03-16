fd04f5b961c9040a4f059f3e896427ef7ff799cf
Daniel Possenriede <possenriede@gmail.com>
2021-03-16 23:14:53 +0100

    adjust vcr_test_path tests

--- tests/testthat/test-vcr_test_path.R
+++ tests/testthat/test-vcr_test_path.R
@@ -1,7 +1,23 @@
 test_that("vcr_test_path works", {
-  expect_match(vcr_test_path("fixtures"), "../fixtures")
-  withr::local_envvar(c("TESTTHAT" = "false"))
-  expect_match(vcr_test_path("fixtures"), "tests/fixtures")
-  expect_message(vcr_test_path("fixtures"), "creating")
-  expect_error(vcr_test_path("", "a"), "non empty")
+  skip_on_cran()
+
+  # setup
+  dir <- file.path(tempdir(), "bunny")
+  invisible(make_pkg(dir))
+  dir.create(file.path(dir, "tests"), recursive = TRUE)
+  withr::local_dir(dir)
+
+  # test
+  expect_message(pth <- vcr_test_path("fixtures"), "creating", fixed = TRUE)
+  expect_match(pth, "tests/fixtures", fixed = TRUE)
+  expect_match(
+    list.dirs(file.path(dir, "tests"), FALSE, FALSE),
+    "fixtures",
+    fixed = TRUE
+  )
+  expect_error(vcr_test_path("", "a"), "non empty", fixed = TRUE)
+  expect_error(vcr_test_path(), "provide", fixed = TRUE)
+
+  # cleanup
+  unlink(dir, TRUE, TRUE)
 })
