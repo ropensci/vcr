12c980c133b1bbe4e9075b0a419481affd180eed
Scott Chamberlain <sckott@fastmail.com>
2021-03-16 10:08:20 -0700

    uncomment test for vcr_test_path

--- tests/testthat/test-vcr_test_path.R
+++ tests/testthat/test-vcr_test_path.R
@@ -1,7 +1,7 @@
 test_that("vcr_test_path works", {
   expect_match(vcr_test_path("fixtures"), "../fixtures")
   withr::local_envvar(c("TESTTHAT" = "false"))
-  # expect_match(vcr_test_path("fixtures"), "tests/fixtures")
+  expect_match(vcr_test_path("fixtures"), "tests/fixtures")
   expect_message(vcr_test_path("fixtures"), "creating")
   expect_error(vcr_test_path("", "a"), "non empty")
 })
