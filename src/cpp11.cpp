6ee2f5579eebd7f6c2f004da97bb1adbad2f4d6b
Scott Chamberlain <sckott@fastmail.com>
2022-07-31 09:03:22 -0700

    cpp11 updates

--- src/cpp11.cpp
+++ src/cpp11.cpp
@@ -3,6 +3,7 @@
 
 
 #include "cpp11/declarations.hpp"
+#include <R_ext/Visibility.h>
 
 // split_str.cpp
 std::vector<std::string> split_str(const std::string& str, int splitLength);
@@ -13,16 +14,13 @@ extern "C" SEXP _vcr_split_str(SEXP str, SEXP splitLength) {
 }
 
 extern "C" {
-/* .Call calls */
-extern SEXP _vcr_split_str(SEXP, SEXP);
-
 static const R_CallMethodDef CallEntries[] = {
     {"_vcr_split_str", (DL_FUNC) &_vcr_split_str, 2},
     {NULL, NULL, 0}
 };
 }
 
-extern "C" void R_init_vcr(DllInfo* dll){
+extern "C" attribute_visible void R_init_vcr(DllInfo* dll){
   R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
   R_useDynamicSymbols(dll, FALSE);
   R_forceSymbols(dll, TRUE);
