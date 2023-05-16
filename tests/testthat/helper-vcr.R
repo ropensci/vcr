6da4637ff62d3b3696d32913e7652524cb97690a
Scott Chamberlain <sckott@fastmail.com>
2023-05-16 09:46:50 -0700

    remove httpbin.org urls in test setup #259

--- tests/testthat/helper-vcr.R
+++ tests/testthat/helper-vcr.R
@@ -68,11 +68,9 @@ sm <- function(x) suppressMessages(x)
 hb <- function(x = NULL) if (is.null(x)) base_url else paste0(base_url, x)
 urls <- c(
   "https://hb.opencpu.org",
-  "https://nghttp2.org/httpbin",
-  "https://httpbin.org",
-  "http://httpbin.org"
+  "https://nghttp2.org/httpbin"
 )
-h <- curl::new_handle(timeout = 10, failonerror = FALSE)
+h <- curl::new_handle(timeout = 5, failonerror = FALSE)
 out <- list()
 for (i in seq_along(urls)) {
   tryCatch({
