596007fe15394f8ee634a4e2a9d3c9657896ca20
Hadley Wickham <h.wickham@gmail.com>
2025-04-21 23:46:44 -0500

    Drop `allow_http_connections_when_no_cassette` option (#352)
    * Drop `allow_http_connections_when_no_cassette` option
    
    This hasn't worked since `current_cassette()` returned an empty object rather than erroring, so I think it's probably just as easy to remove
    
    * Update NEWS.md
    
    Co-authored-by: Scott Chamberlain <sckott@fastmail.com>
    
    ---------
    
    Co-authored-by: Scott Chamberlain <sckott@fastmail.com>

--- R/errors.R
+++ R/errors.R
@@ -316,7 +316,6 @@ UnhandledHTTPRequestError <- R6::R6Class(
       x <- c(
         "try_debug_logger",
         "use_a_cassette",
-        "allow_http_connections_when_no_cassette",
         "ignore_request"
       )
       lapply(x, self$suggestion_for)
