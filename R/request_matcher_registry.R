ac3821117a39c0119f7dd7787a8d6471875fe139
Hadley Wickham <h.wickham@gmail.com>
2025-04-23 16:27:39 -0700

    Complete request_matcher tests

--- R/request_matcher_registry.R
+++ R/request_matcher_registry.R
@@ -27,12 +27,19 @@ request_matches_one <- function(type, req1, req2) {
     ),
     body = identical(req1$body, req2$body),
     headers = identical(req1$headers, req2$headers),
-    host = identical(req1$host, req2$host),
-    path = identical(sub("/$", "", req1$path), sub("/$", "", req2$path)),
-    query = identical(
-      curl::curl_unescape(req1$query),
-      curl::curl_unescape(req2$query)
-    ),
+    host = identical(url_host(req1$uri), url_host(req2$uri)),
+    path = identical(url_path(req1$uri), url_path(req2$uri)),
+    query = identical(url_query(req1$uri), url_query(req2$uri)),
     cli::cli_abort("Unsupported request matcher {.str {type}}.")
   )
+}
+
+url_path <- function(x) {
+  sub("/$", "", curl::curl_parse_url(x)$path)
+}
+url_query <- function(x) {
+  curl::curl_parse_url(x)$query
+}
+url_host <- function(x) {
+  curl::curl_parse_url(x)$host
 }
--- tests/testthat/test-request_matcher_registry.R
+++ tests/testthat/test-request_matcher_registry.R
@@ -1,3 +1,16 @@
+test_that("multiple matchers requires all to match", {
+  expect_true(request_matches(
+    list(method = "GET", uri = "http://a.com"),
+    list(method = "GET", uri = "http://a.com"),
+    c("method", "uri")
+  ))
+  expect_false(request_matches(
+    list(method = "GET", uri = "http://a.com"),
+    list(method = "GET", uri = "http://b.com"),
+    c("method", "uri")
+  ))
+})
+
 test_that("can match by method", {
   expect_true(request_matches_one(
     "method",
@@ -21,6 +34,60 @@ test_that("can match by url", {
     "uri",
     list(uri = "http://a.com"),
     list(uri = "http://b.com")
+  ))
+})
+
+test_that("can match by host", {
+  expect_true(request_matches_one(
+    "host",
+    list(uri = "http://a.com/foo"),
+    list(uri = "http://a.com/bar")
+  ))
+  expect_false(request_matches_one(
+    "host",
+    list(uri = "http://a.com/foo"),
+    list(uri = "http://b.com/foo")
+  ))
+})
+
+test_that("can match by path", {
+  expect_true(request_matches_one(
+    "path",
+    list(uri = "http://a.com/foo"),
+    list(uri = "http://b.com/foo")
+  ))
+  expect_true(request_matches_one(
+    "path",
+    list(uri = "http://a.com/"),
+    list(uri = "http://b.com")
+  ))
+  expect_true(request_matches_one(
+    "path",
+    list(uri = "http://a.com/foo"),
+    list(uri = "http://b.com/foo/")
+  ))
+  expect_false(request_matches_one(
+    "path",
+    list(uri = "http://a.com/foo"),
+    list(uri = "http://a.com/bar")
+  ))
+})
+
+test_that("can match by query", {
+  expect_true(request_matches_one(
+    "query",
+    list(uri = "http://a.com/foo?foo=bar"),
+    list(uri = "http://a.com/bar?foo=bar")
+  ))
+  expect_true(request_matches_one(
+    "query",
+    list(uri = "http://a.com/foo?foo=%C2%B5"),
+    list(uri = "http://a.com/bar?foo=\u00b5")
+  ))
+  expect_false(request_matches_one(
+    "query",
+    list(uri = "http://a.com/foo?foo=bar"),
+    list(uri = "http://a.com/foo?foo=baz")
   ))
 })
 
