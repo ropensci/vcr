ef1c2a0a5d0bbaf2426547fc7d53fc02fd3be6ab
Hadley Wickham <h.wickham@gmail.com>
2025-04-24 08:55:55 -0700

    Simplify httr2 response generation

--- R/request_handler-httr2.R
+++ R/request_handler-httr2.R
@@ -56,7 +56,23 @@ RequestHandlerHttr2 <- R6::R6Class(
     },
 
     on_stubbed_by_vcr_request = function() {
-      serialize_to_httr2(self$request, super$get_stubbed_response(self$request))
+      vcr_response <- super$get_stubbed_response(self$request)
+
+      if (is.null(vcr_response$body)) {
+        body <- NULL
+      } else if (is.raw(vcr_response$body)) {
+        body <- vcr_response$body
+      } else {
+        body <- charToRaw(vcr_response$body)
+      }
+
+      httr2::response(
+        status_code = vcr_response$status$status_code %||% 200,
+        url = self$request_original$url,
+        method = webmockr:::req_method_get_w(self$request_original),
+        headers = vcr_response$headers,
+        body = body
+      )
     },
 
     on_recordable_request = function() {
@@ -74,54 +90,6 @@ RequestHandlerHttr2 <- R6::R6Class(
     }
   )
 )
-
-# generate actual httr2 response
-# request <- httr2::request("https://hb.opencpu.org/301")
-#
-serialize_to_httr2 <- function(request, response) {
-  # request
-  req <- webmockr::RequestSignature$new(
-    method = request$method,
-    uri = request$uri,
-    options = list(
-      body = request$body %||% NULL,
-      headers = request$headers %||% NULL,
-      proxies = NULL,
-      auth = NULL,
-      disk = if (inherits(response$body, "httr2_path")) response$body %||% NULL,
-      fields = request$fields %||% NULL,
-      output = request$output %||% NULL
-    )
-  )
-
-  # response
-  resp <- webmockr::Response$new()
-  resp$set_url(request$uri)
-  resp$set_body(response$body, inherits(response$body, "httr2_path"))
-  resp$set_request_headers(request$headers, capitalize = FALSE)
-  resp$set_response_headers(response$headers, capitalize = FALSE)
-  resp$set_status(status = response$status$status_code %||% 200)
-
-  # generate httr2 response
-  resp <- webmockr::build_httr2_response(as_httr2_request(req), resp)
-  attr(resp$headers, "redact") <- character()
-  resp
-}
-
-as_httr2_request <- function(x) {
-  structure(
-    list(
-      url = x$url$url,
-      method = toupper(x$method),
-      headers = x$headers,
-      body = x$body,
-      fields = x$fields,
-      options = x$options,
-      policies = x$policies
-    ),
-    class = "httr2_request"
-  )
-}
 
 #' @note adapted from httr2:::req_body_get
 #' @keywords internal
