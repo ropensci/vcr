9ef57d0be29f7f108b588a5887df1e0e7e8f4988
Hadley Wickham <h.wickham@gmail.com>
2025-04-24 10:05:26 -0700

    Move all serializers into one file

--- R/serializer.R
+++ R/serializer.R
@@ -1,3 +1,13 @@
+serializer_fetch <- function(type, path, name, preserve_bytes = FALSE) {
+  switch(
+    type,
+    json = JSON$new(path, name, preserve_bytes = preserve_bytes),
+    yaml = YAML$new(path, name, preserve_bytes = preserve_bytes),
+    cli::cli_abort("Unsupported cassette serializer {.str {type}}.")
+  )
+}
+
+
 Serializer <- R6::R6Class(
   "Serializer",
   public = list(
@@ -15,11 +25,53 @@ Serializer <- R6::R6Class(
   )
 )
 
-serializer_fetch <- function(type, path, name, preserve_bytes = FALSE) {
-  switch(
-    type,
-    json = JSON$new(path, name, preserve_bytes = preserve_bytes),
-    yaml = YAML$new(path, name, preserve_bytes = preserve_bytes),
-    cli::cli_abort("Unsupported cassette serializer {.str {type}}.")
+JSON <- R6::R6Class(
+  "JSON",
+  inherit = Serializer,
+  public = list(
+    initialize = function(path, name, preserve_bytes = FALSE) {
+      super$initialize(path, name, ".json", preserve_bytes = preserve_bytes)
+    },
+
+    serialize = function(data) {
+      out <- encode_interactions(data, self$preserve_bytes)
+      jsonlite::write_json(
+        out,
+        self$path,
+        auto_unbox = TRUE,
+        pretty = vcr_c$json_pretty
+      )
+    },
+
+    deserialize = function() {
+      str <- sensitive_put_back(readLines(self$path))
+      interactions <- jsonlite::fromJSON(str, FALSE)
+      interactions <- query_params_put_back(interactions)
+      interactions <- decode_interactions(interactions, self$preserve_bytes)
+      interactions
+    }
   )
-}
+)
+
+YAML <- R6::R6Class(
+  "YAML",
+  inherit = Serializer,
+  public = list(
+    initialize = function(path, name, preserve_bytes = FALSE) {
+      super$initialize(path, name, ".yml", preserve_bytes = preserve_bytes)
+    },
+
+    serialize = function(data) {
+      out <- encode_interactions(data, self$preserve_bytes)
+      yaml::write_yaml(out, self$path)
+    },
+
+    deserialize = function() {
+      str <- sensitive_put_back(readLines(self$path, encoding = "UTF-8"))
+      interactions <- yaml::yaml.load(str)
+      interactions <- query_params_put_back(interactions)
+      interactions <- decode_interactions(interactions, self$preserve_bytes)
+      interactions
+    }
+  )
+)
