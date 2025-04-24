866f1fadb4ad64796502dc1bb4ed9d3210aab61d
Scott Chamberlain <sckott@fastmail.com>
2025-04-24 14:03:05 -0700

    add test-errors.R back

---
+++ tests/testthat/test-errors.R
@@ -0,0 +1,141 @@
+test_that("UnhandledHTTPRequestError fails well", {
+  request <- Request$new("post", hb("/post?a=5"), "", list(foo = "bar"))
+
+  z <- UnhandledHTTPRequestError$new(request)
+  expect_error(
+    z$construct_message(),
+    "There is currently no cassette in use"
+  )
+
+  local_cassette("asdsfd", warn_on_empty = FALSE)
+  # # types
+  expect_error(
+    UnhandledHTTPRequestError$new(5),
+    "request must be of class Request"
+  )
+})
+
+test_that("UnhandledHTTPRequestError works as expected", {
+  request <- Request$new("post", hb("/post?a=5"), "", list(foo = "bar"))
+  local_cassette("turtle", warn_on_empty = FALSE)
+
+  a <- UnhandledHTTPRequestError$new(request)
+
+  expect_s3_class(a, "UnhandledHTTPRequestError")
+  expect_s3_class(a$cassette, "Cassette")
+  expect_equal(a$cassette$name, "turtle")
+  expect_type(a$construct_message, "closure")
+
+  expect_error(
+    a$construct_message(),
+    "An HTTP request has been made that vcr does not know how to handle"
+  )
+})
+
+test_that("UnhandledHTTPRequestError works as expected", {
+  ## API key in query param
+  withr::local_envvar(
+    FOO_BAR = "2k2k2k288gjrj2i21i",
+    HELLO_WORLD = "asdfadfasfsfs239823n23"
+  )
+  local_vcr_configure(
+    filter_sensitive_data = list(
+      "<<foo_bar_key>>" = Sys.getenv("FOO_BAR"),
+      "<<hello_world_key>>" = Sys.getenv("HELLO_WORLD")
+    )
+  )
+  url <- paste0(
+    hb("/get?api_key="),
+    Sys.getenv("FOO_BAR"),
+    "&other_secret=",
+    Sys.getenv("HELLO_WORLD")
+  )
+  request <- Request$new("get", url, "")
+  local_cassette("bunny", warn_on_empty = FALSE)
+
+  a <- UnhandledHTTPRequestError$new(request)
+
+  expect_s3_class(a, "UnhandledHTTPRequestError")
+  expect_s3_class(a$cassette, "Cassette")
+  expect_equal(a$cassette$name, "bunny")
+  expect_type(a$construct_message, "closure")
+
+  expect_error(
+    a$construct_message(),
+    "An HTTP request has been made that vcr does not know how to handle"
+  )
+  expect_error(
+    a$construct_message(),
+    "<<foo_bar_key>>"
+  )
+  expect_error(
+    a$construct_message(),
+    "<<hello_world_key>>"
+  )
+})
+
+test_that("UnhandledHTTPRequestError works as expected", {
+  ## API key in header
+  withr::local_envvar(FOO_BAR = "2k2k2k288gjrj2i21i")
+  local_vcr_configure(
+    dir = withr::local_tempdir(),
+    filter_sensitive_data = list("<<foo_bar_key>>" = Sys.getenv("FOO_BAR")),
+  )
+  url <- hb("/get")
+  request <- Request$new("get", url, "", list(api_key = Sys.getenv("FOO_BAR")))
+  local_cassette(
+    "frog",
+    match_requests_on = c("method", "uri", "headers"),
+    warn_on_empty = FALSE
+  )
+
+  a <- UnhandledHTTPRequestError$new(request)
+
+  expect_s3_class(a, "UnhandledHTTPRequestError")
+  expect_s3_class(a$cassette, "Cassette")
+  expect_equal(a$cassette$name, "frog")
+  expect_type(a$construct_message, "closure")
+
+  expect_error(
+    a$construct_message(),
+    "An HTTP request has been made that vcr does not know how to handle"
+  )
+  expect_error(
+    a$construct_message(),
+    "api_key: <<foo_bar_key>>"
+  )
+})
+
+test_that("UnhandledHTTPRequestError works as expected", {
+  ## API key not found or empty (i.e., "")
+  withr::local_envvar(HELLO_MARS = "asdfadfasfsfs239823n23")
+  local_vcr_configure(
+    filter_sensitive_data = list(
+      "<<bar_foo_key>>" = Sys.getenv("BAR_FOO"),
+      "<<hello_mars_key>>" = Sys.getenv("HELLO_MARS")
+    )
+  )
+  url <- paste0(hb("/get?api_key="), Sys.getenv("HELLO_MARS"))
+  request <- Request$new("get", url, "")
+  local_cassette("bunny2", warn_on_empty = FALSE)
+
+  a <- UnhandledHTTPRequestError$new(request)
+
+  expect_s3_class(a, "UnhandledHTTPRequestError")
+  expect_s3_class(a$cassette, "Cassette")
+  expect_equal(a$cassette$name, "bunny2")
+  expect_type(a$construct_message, "closure")
+
+  expect_error(
+    a$construct_message(),
+    "An HTTP request has been made that vcr does not know how to handle"
+  )
+  # we should see this string in the error message
+  expect_error(
+    a$construct_message(),
+    "<<hello_mars_key>>"
+  )
+  # we should not see this string in the error message b/c it's empty/missing
+  res <- tryCatch(a$construct_message(), error = function(e) e)
+  expect_false(grepl("<<bar_foo_key>>", res$message))
+})
