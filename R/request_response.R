475a63d84d303e2ef00fbae9e7093adc1693df7e
Hadley Wickham <h.wickham@gmail.com>
2025-05-01 12:40:08 -0500

    Don't document/export internal functions/classes (#443)
    I left the Cassette class exported/documented for now, since it is technically part of the exported interface of the package because it's returned by `use_cassette()` etc. I could be persauded that it should be removed too (but that should wait until after #429 to avoid a bunch of merge conflicts)
    
    Fixes #374.

--- R/request_response.R
+++ R/request_response.R
@@ -1,25 +1,3 @@
-#' request and response summary methods
-#'
-#' @export
-#' @name request_response
-#' @keywords internal
-#' @param request A `vcr_request` object
-#' @param request_matchers (character) a vector of matchers.
-#' Default: `""`
-#' @param response A `vcr_response` object
-#' @return character string, of either request or response
-#' @details By default, method and uri are included
-#' in the request summary - if body and/or headers are
-#' specified in `request_matchers`, then they are also
-#' included
-#'
-#' HTTP status code and response body are included in the
-#' response summary. The response body is truncated to a
-#' max of 80 characters
-#'
-#' In `response_summary()` we use [gsub] with `useBytes=TRUE` to avoid
-#' problems sometimes seen with multibyte strings - this shouldn't affect
-#' your data/etc. as this is only for printing a summary of the response
 request_summary <- function(request, request_matchers = "") {
   stopifnot(inherits(request_matchers, "character"))
   atts <- c(request$method, request$uri)
@@ -32,8 +10,6 @@ request_summary <- function(request, request_matchers = "") {
   paste0(atts, collapse = " ")
 }
 
-#' @export
-#' @rdname request_response
 response_summary <- function(response) {
   stopifnot(inherits(response, "vcr_response"))
 
