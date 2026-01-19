2546a97ef0070bcade9080f92b0521dc0a2a39af
Maëlle Salmon <maelle.salmon@yahoo.se>
2026-01-19 12:21:19 +0100

    rm example since we have a real test now

--- R/roxygen-example.R
+++
@@ -1,9 +0,0 @@
-#' @rdname vcr-example
-#' @name vcr-example
-#' @title Trying out our custom tag
-#' @keywords internal
-#' @examplesVCR bla
-#' if (rlang::is_installed("httr2")) {
-#'   httr2::request("http://r-project.org")
-#' }
-NULL
